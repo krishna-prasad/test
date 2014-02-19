2733c0dca3f0c0748dcc7f8b6e307f012015ada7
Krishna Prasad <krishna@Krishnas-Mac-mini.local>
2014-02-19 16:31:10 +0530

    different files

--- testClass.php
+++ testClass.php
@@ -4,4 +4,14 @@
  * User: krishna
  * Date: 19/02/14
  * Time: 4:20 PM
- */ 
+ */
+class testClass
+{
+    public function printName() {
+        echo 'hello krishna prasad';
+    }
+
+}
+
+$obj = new testClass();
+$obj->printName();
